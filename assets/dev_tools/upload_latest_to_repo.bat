0ae0f91e0458270f377033db0cbcaf87de0bc725
Mythical-Github <MythicalData@gmail.com>
2024-11-12 17:33:48 -0500

    test

--- assets/dev_tools/upload_latest_to_repo.bat
+++ assets/dev_tools/upload_latest_to_repo.bat
@@ -2,11 +2,11 @@
 
 cd /d %~dp0
 
-set "repo_branch=dev"
+set "repo_branch=main"
 
-set "py_project_dev_tools_exe=%CD%/py_project_dev_tools.exe"
+set "py_project_dev_tools_exe=%CD%\py_project_dev_tools.exe"
 
-set "toml=%CD%/../../pyproject.toml"
+set "toml=%CD%\..\..\pyproject.toml"
 
 "%py_project_dev_tools_exe%" upload_latest_to_repo "%toml%" %repo_branch%
 
